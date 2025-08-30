136689778781e058b68084e23206ddb9cc0c96f0
Mohamed <mohex321@hotmail.com>
2025-08-29 17:24:04 -0700

    t

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom'; // أضف هذا الاستيراد
 import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router> {/* غلف App بـ Router هنا */}
+      <App />
+    </Router>
   </React.StrictMode>
 );
